25e7f35342f4d52a266b39b6a445ffdc37cac767
Frederik Cornelius Skovgaard <fred56b8@elevcampus.dk>
2022-10-11 15:03:59 +0200

    Add project files.

---
+++ DataLayer/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int CardId { get; set; }
+        public int UserInformationId { get; set; }
+        public int RoleId { get; set; }
+
+        public Cart Cart { get; set; }
+        public UserInformation UserInformation { get; set; }
+        public Role Role { get; set; }
+    }
+}
